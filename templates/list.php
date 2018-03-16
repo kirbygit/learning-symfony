7f465157825e1041122ec0110d045668039e91d3
kboygit <heybisayangdako@gmail.com>
2018-03-16 15:57:49 -0700

    isolating the layout

--- templates/list.php
+++ templates/list.php
@@ -1,30 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>LOP</title>
-  </head>
-  <body>
-    <h1>List of Posts</h1>
-    <ul>
-      <?php foreach ($posts as $post): ?>
-        <li>
-          <a href="/show.php?id=<?= $post['id'] ?>">
-            <?= $post['title'] ?>
-          </a>
-        </li>
-      <?php endforeach ?>
-    </ul>
-  </body>
-</html>
+<?php $title = 'List of Posts' ?>
 
-<?php
-$link = null;
-?>
+<?php ob_start() ?>
+  <h1>List of Posts</h1>
+  <ul>
+    <?php foreach ($posts as $post): ?>
+      <li>
+        <a href="/show.php?id=<?= $post['id'] ?>">
+          <?= $post['title'] ?>
+        </a>
+      </li>
+    <?php endforeach ?>
+  </ul>
+  <?php $content = ob_get_clean() ?>
 
-<!--
-Problems need to be addressed in the long-run
-- No error checking
-- Poor Organization
-- Difficult to reuse code
- -->
+  <?php include 'layout.php' ?>
