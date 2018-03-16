27ef5b70d7b9f8360f6bcefbcd2eebc9cfea69f3
kboygit <heybisayangdako@gmail.com>
2018-03-16 15:38:47 -0700

    isolating the application domain logic

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
-// index.php
-$link = new PDO("mysql:host=localhost;dbname=blog_db", 'myuser', 'mypassword');
+  require 'model.php';
 
-$result = $link->query('SELECT id, title FROM post');
+  $posts = get_all_posts();
 
-$posts = array();
-while($row = $result->fetch(PDO::FETCH_ASSOC))
-{
-  $posts[] = $row;
-}
-
-$link = null;
-
-require 'templates/list.php';
+  require 'templates/list.php';
 ?>
