7bd01343148d73d40d5b021a9122b0a6ebfc7fbd
kboygit <heybisayangdako@gmail.com>
2018-03-16 16:17:17 -0700

    added a blog show page

--- model.php
+++ model.php
@@ -26,4 +26,20 @@
 
     return $posts;
   }
+
+  function get_post_by_id($id)
+  {
+    $link = open_database_connection();
+
+    $query = 'SELECT created_at, title, body FROM post WHERE id=:id';
+    $statement = $link->prepare($query);
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    close_database_connection($link);
+
+    return $row;
+  }
 ?>
