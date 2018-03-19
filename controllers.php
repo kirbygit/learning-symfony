3c20a90103cf63536e1fa0e4d641c3201b7f37de
kboygit <heybisayangdako@gmail.com>
2018-03-19 11:21:14 -0700

    improve blog representation, added render_template function

--- controllers.php
+++ controllers.php
@@ -1,13 +1,30 @@
 <?php
+  use Symfony\Component\HttpFoundation\Response;
+  
   function list_action()
   {
     $posts = get_all_posts();
-    require 'templates/list.php';
+    $html = render_template('templates/list.php', array('posts' => $posts));
+
+    return new Response($html);
   }
 
   function show_action($id)
   {
-    $post = get_post_by_id($_GET['id']);
-    require 'templates/show.php';
+    $post = get_post_by_id($id);
+    $html = render_template('templates/show.php', array('posts' => $posts));
+
+    return new Response($html);
+  }
+
+  // helper function to render templates
+  function render_template($path, array $args)
+  {
+    extract($args);
+    ob_start();
+    require $path;
+    $html = ob_get_clean();
+
+    return $html;
   }
 ?>
